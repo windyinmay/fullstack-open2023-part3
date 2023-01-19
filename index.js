fdbe2328d0f82700b6ffb03f311883465bb4c59d
windyinmay <quynhanhmay.butter@gmail.com>
2023-01-19 23:01:28 +0200

    ex 3.2 to ex 3.4 done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http')
 const express = require('express')
+const { response } = require('express')
 // const app = http.createServer((request, response) => {
 //     response.writeHead(200, {'Content-Type': 'application/json'})
 //     response.end(JSON.stringify(persons))
@@ -30,13 +31,40 @@ let persons = [
 ]
 
 app.get('/', (req, res) => {
-    res.send('<h1>Testing<h1>')
+    res.send('<h1>Fullstack Open - Part3<h1>')
 })
 
 app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    console.log(person)
+    if(person) {
+        res.json(person)
+    } else {
+        // res.statusMessage = ""
+        res.status(404).send('NOT FOUND')
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p => p.id !== id)
+
+    res.status(204).end()
+})
+
+//show info
+app.get('/info', (req,res) => {
+    const date = new Date()
+    res.send(`Phonebook has info for ${persons.length} people
+    <br/> <br/>
+    ${date}`)
+})
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server is running on port ${PORT}`)
