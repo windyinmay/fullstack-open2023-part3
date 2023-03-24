f4b6eb855db7cae8483133fa018d5d3d74c732d7
windyinmay <quynhanhmay.butter@gmail.com>
2023-03-24 17:01:21 +0200

    done ex 3.21 and 3.22

--- mongo.js
+++ mongo.js
@@ -1,48 +1,49 @@
-const mongoose = require("mongoose");
-const password = process.argv[2];
+const mongoose = require('mongoose')
+const password = process.argv[2]
 
-const url = `mongodb+srv://windyinmay:${password}@windyinmay.lymjfsp.mongodb.net/phonebook-part3?retryWrites=true&w=majority`;
+const url = `mongodb+srv://windyinmay:${password}@windyinmay.lymjfsp.mongodb.net/phonebook-part3?retryWrites=true&w=majority`
 
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
 
 const phonebookSchema = new mongoose.Schema({
   //   id: Number,
   name: String,
   number: String,
-});
+})
 
-const PhoneBook = mongoose.model("PhoneBook", phonebookSchema);
+const PhoneBook = mongoose.model('PhoneBook', phonebookSchema)
 
-PhoneBook.name = process.argv[3];
-PhoneBook.number = process.argv[4];
+PhoneBook.name = process.argv[3]
+PhoneBook.number = process.argv[4]
 
 const phonebook = new PhoneBook({
   //   id: Math.floor(Math.random() * 10000000),
   name: process.argv[3],
   number: process.argv[4],
-});
+})
 
 if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-if (process.argv.length == 3) {
+if (process.argv.length === 3) {
   PhoneBook.find({}).then((result) => {
-    console.log(`Phonebook:`);
+    console.log('Phonebook:')
     result.forEach((phonebook) => {
-      console.log(`${phonebook.name} ${phonebook.number}`);
-      mongoose.connection.close();
-    });
-  });
+      console.log(`${phonebook.name} ${phonebook.number}`)
+      mongoose.connection.close()
+    })
+  })
 }
 
 if (process.argv.length > 3) {
-  phonebook.save().then((result) => {
+  // eslint-disable-next-line no-unused-vars
+  phonebook.save().then((_result) => {
     console.log(
       `added ${phonebook.name} number ${phonebook.number} to phonebook`
-    );
-    mongoose.connection.close();
-  });
+    )
+    mongoose.connection.close()
+  })
 }
