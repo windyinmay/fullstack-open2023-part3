be2f9dfa7577f94ad187a56db13221467fa737bd
windyinmay <quynhanhmay.butter@gmail.com>
2023-03-24 15:35:29 +0200

    done ex 3.20

--- models/person.js
+++ models/person.js
@@ -20,12 +20,22 @@ const phonebookSchema = new mongoose.Schema({
   name: {
     type: String,
     minLength: 3,
-    required: true,
+    required: [true, "Name is required"],
   },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function (v) {
+        return /\d{2,3}-\d{5,}/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, "User phone number required"],
+  },
 });
 phonebookSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
+  transform: (_document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
